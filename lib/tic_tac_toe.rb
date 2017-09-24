d69ccef7e629fb6a40eacc3e261cb49927e8e82d
Kayla Hansen <shiverr@gmail.com>
2017-09-24 04:19:38 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -62,6 +62,14 @@ end
   @board.count{|token| token == "X" || token == "O"}
   end
 
+  def won?
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
+end
+
 
 
 end
