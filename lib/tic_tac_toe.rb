922d92c7eff4ab099cedcd1d814b333059bd327f
Kayla Hansen <shiverr@gmail.com>
2017-09-24 04:20:00 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -66,7 +66,7 @@ end
   WIN_COMBINATIONS.detect do |combo|
     board[combo[0]] == board[combo[1]] &&
     board[combo[1]] == board[combo[2]] &&
-    position_taken?(board, combo[0])
+    position_taken?(combo[0])
   end
 end
 
